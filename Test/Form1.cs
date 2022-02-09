c192974767865fd2b18a62cb186fc52f647acece
Vít Novotný <novotvit@hotmail.com>
2022-02-09 08:32:17 +0100

    Soucin

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -16,5 +16,13 @@ namespace Test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int soucin = a * b;
+            label1.Text = string.Format("Soucin cisel {0} a {1} je {2}", a, b, soucin);
+        }
     }
 }
